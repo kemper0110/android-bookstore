bee161be571f34a0f51e4b39fe99165e70e86606
Danil <danil2002.2043@gmail.com>
2024-03-02 13:45:22 +0300

    model serial name

--- app/src/main/java/donstu/danil/bookstore/Book.kt
+++ app/src/main/java/donstu/danil/bookstore/Book.kt
@@ -1,12 +1,14 @@
 package donstu.danil.bookstore
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Book(
     val id: Int,
     val name: String,
-    val type_id: String,
+    @SerialName("type_id")
+    val typeId: String,
     val price: Int,
     val rating: Int,
     val image: String
