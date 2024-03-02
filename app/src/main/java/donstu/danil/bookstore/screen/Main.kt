5d74f5111108e3c939ae0e961c74ece6e4d536ab
Danil <danil2002.2043@gmail.com>
2024-03-02 13:46:11 +0300

    main layout page

---
+++ app/src/main/java/donstu/danil/bookstore/screen/Main.kt
@@ -0,0 +1,21 @@
+package donstu.danil.bookstore.screen
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import donstu.danil.bookstore.books
+import donstu.danil.bookstore.ui.component.BookCard
+
+@Composable
+fun Main(modifier: Modifier) {
+    LazyVerticalGrid(
+        modifier = modifier.fillMaxHeight(),
+        columns = GridCells.Fixed(2)
+    ) {
+        items(books) { book -> BookCard(book) }
+    }
+}
