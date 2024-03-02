5ab0d8f3fd6527754146c0bff5120eaebb67d205
Danil <danil2002.2043@gmail.com>
2024-03-03 00:23:28 +0300

    books page fetching

--- app/src/main/java/donstu/danil/bookstore/screen/Books.kt
+++ app/src/main/java/donstu/danil/bookstore/screen/Books.kt
@@ -1,20 +1,99 @@
 package donstu.danil.bookstore.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import donstu.danil.bookstore.books
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import donstu.danil.bookstore.BookModel
+import donstu.danil.bookstore.R
 import donstu.danil.bookstore.ui.component.BookCard
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+
+@Serializable
+data class BooksResponse(
+    val books: Array<BookModel>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BooksResponse
+
+        return books.contentEquals(other.books)
+    }
+
+    override fun hashCode(): Int {
+        return books.contentHashCode()
+    }
+}
 
 @Composable
 fun Books(modifier: Modifier, onNavigate: (Int) -> Unit) {
-    LazyVerticalGrid(
-        modifier = modifier.fillMaxHeight(),
-        columns = GridCells.Fixed(2)
-    ) {
-        items(books) { book -> BookCard(book) { onNavigate(book.id) } }
+    val gridState = rememberLazyGridState()
+
+    val context = LocalContext.current
+    var text by remember { mutableStateOf("no text") }
+    val queue by remember { mutableStateOf(Volley.newRequestQueue(context)) }
+
+    var booksResponse by remember { mutableStateOf<BooksResponse?>(null) }
+    val books = remember(booksResponse) { booksResponse?.books }
+
+    LaunchedEffect(null) {
+        val stringRequest = object : StringRequest(
+            Method.GET, context.getString(R.string.api_url),
+            { response ->
+                text = "Response is: ${response.substring(0, response.length.coerceAtMost(500))}"
+                booksResponse = Json.decodeFromString<BooksResponse>(response)
+                println(booksResponse)
+            },
+            { error -> text = error.message.toString() }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                return hashMapOf("Accept" to "application/json")
+            }
+        }
+        queue.add(stringRequest)
+    }
+
+    if (books != null) {
+        LazyVerticalGrid(
+            state = gridState,
+            modifier = modifier.fillMaxHeight(),
+            columns = GridCells.Fixed(2)
+        ) {
+            items(
+                books, key = { book -> book.id }) { book ->
+                BookCard(book) { onNavigate(book.id) }
+            }
+        }
+    } else {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .padding(horizontal = 6.dp, vertical = 3.dp),
+            )
+        }
     }
 }
--- app/src/main/java/donstu/danil/bookstore/ui/component/BookCard.kt
+++ app/src/main/java/donstu/danil/bookstore/ui/component/BookCard.kt
@@ -7,42 +7,60 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
 import donstu.danil.bookstore.BookModel
 import donstu.danil.bookstore.R
-import donstu.danil.bookstore.books
 import donstu.danil.bookstore.ui.theme.BookstoreTheme
 
 @Composable
 fun BookCard(book: BookModel, onClick: () -> Unit) {
-    val image = R.drawable::class.java.fields.filter { it.name.equals(book.image.split(".").first()) }.map {
-        LocalContext.current.resources.getIdentifier(
-            it.name,
-            "drawable",
-            LocalContext.current.packageName
-        )
-    }.first()
+    val context = LocalContext.current
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.clickable(onClick = onClick)
+        modifier = Modifier.clickable(onClick = onClick).padding(4.dp, 8.dp)
     ) {
-        Image(painter = painterResource(id = image), "Абоба")
+        Box (
+            contentAlignment = Alignment.Center
+        ) {
+            val painter = rememberAsyncImagePainter(context.getString(R.string.image_url) + book.image)
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+                    .clip(RoundedCornerShape(4.dp))
+                ,
+                painter = painter,
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+            )
+            if (painter.state is AsyncImagePainter.State.Loading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .padding(horizontal = 6.dp, vertical = 3.dp),
+                )
+            }
+        }
         Column(
             Modifier
                 .padding(Dp(8F))
@@ -68,7 +86,7 @@ fun BookCard(book: BookModel, onClick: () -> Unit) {
 fun BookCardListPreview() {
     BookstoreTheme {
         LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-            items(books) { book -> BookCard(book, {}) }
+//            items(offlineBooks) { book -> BookCard(book, {}) }
         }
     }
 }
@@ -79,7 +97,7 @@ fun BookCardListPreview() {
 fun BookCardPreview() {
     BookstoreTheme {
         Box(modifier = Modifier.width(240.dp)) {
-            BookCard(books[0], {})
+//            BookCard(offlineBooks[0], {})
         }
     }
 }
