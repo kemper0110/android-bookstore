51da742606ebe855b3f381d15fbe4fbdbe1107dc
Danil <danil2002.2043@gmail.com>
2024-03-04 23:40:01 +0300

    reworked routing (idk is it better)

--- app/src/main/java/donstu/danil/bookstore/MainActivity.kt
+++ app/src/main/java/donstu/danil/bookstore/MainActivity.kt
@@ -23,18 +23,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.android.volley.RequestQueue
@@ -48,6 +44,41 @@ import donstu.danil.bookstore.ui.theme.BookstoreTheme
 val LocalRequestQueue =
     compositionLocalOf<RequestQueue> { error("`RequestQueue` is not initialized in `compositionLocalOf`") }
 
+@Composable
+fun BottomBar(navigate: (url: String) -> Unit, selectedUrl: String) {
+    BottomAppBar(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        contentColor = MaterialTheme.colorScheme.primary,
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconButton(onClick = { navigate("/") }) {
+                Icon(
+                    Icons.Filled.Home,
+                    contentDescription = null,
+                    tint = if (selectedUrl == "/") Color.White else LocalContentColor.current
+                )
+            }
+            IconButton(onClick = { navigate("/about-store") }) {
+                Icon(
+                    Icons.Filled.ShoppingCart,
+                    contentDescription = null,
+                    tint = if (selectedUrl == "/about-store") Color.White else LocalContentColor.current
+                )
+            }
+            IconButton(onClick = { navigate("/about-me") }) {
+                Icon(
+                    Icons.Filled.Face,
+                    contentDescription = null,
+                    tint = if (selectedUrl == "/about-me") Color.White else LocalContentColor.current
+                )
+            }
+        }
+    }
+}
+
 class MainActivity : ComponentActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     @OptIn(ExperimentalMaterial3Api::class)
@@ -56,86 +87,77 @@ class MainActivity : ComponentActivity() {
         setContent {
             val requestQueue = Volley.newRequestQueue(applicationContext)
             CompositionLocalProvider(LocalRequestQueue provides requestQueue) {
+                val navController = rememberNavController()
                 BookstoreTheme {
-                    val navController = rememberNavController()
-
-                    data class NavItem(
-                        val icon: ImageVector,
-                        val path: String,
-                        val title: String
-                    )
-
-                    val navItems = remember {
-                        arrayListOf(
-                            NavItem(icon = Icons.Filled.Home, path = "/", title = "Книги"),
-                            NavItem(
-                                icon = Icons.Filled.ShoppingCart,
-                                path = "/about-store",
-                                title = "О магазине"
-                            ),
-                            NavItem(
-                                icon = Icons.Filled.Face,
-                                path = "/about-me",
-                                title = "Об авторе"
-                            ),
-                        )
-                    }
-                    val navBackStackEntry by navController.currentBackStackEntryAsState()
-                    val currentDestination = navBackStackEntry?.destination
-                    val currentNavItem =
-                        navItems.find { navItem -> currentDestination?.hierarchy?.any { it.route == navItem.path } == true }
-                    Scaffold(
-                        topBar = {
-                            TopAppBar(
-                                colors = topAppBarColors(
-                                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                    titleContentColor = MaterialTheme.colorScheme.primary,
-                                ),
-                                title = {
-                                    currentNavItem?.title?.let { Text(it) }
+                    NavHost(navController, startDestination = "/", Modifier.fillMaxHeight()) {
+                        composable("/") {
+                            Scaffold(
+                                topBar = {
+                                    TopAppBar(
+                                        colors = topAppBarColors(
+                                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                            titleContentColor = MaterialTheme.colorScheme.primary,
+                                        ),
+                                        title = { Text("Книги") }
+                                    )
+                                },
+                                bottomBar = {
+                                    BottomBar({ navController.navigate(it) }, "/")
                                 }
-                            )
-                        },
-                        bottomBar = {
-                            BottomAppBar(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                contentColor = MaterialTheme.colorScheme.primary,
-                            ) {
-                                Row(
-                                    horizontalArrangement = Arrangement.SpaceAround,
-                                    modifier = Modifier.fillMaxWidth()
-                                ) {
-                                    navItems.forEach { navItem ->
-                                        val selected =
-                                            currentDestination?.hierarchy?.any { it.route == navItem.path } == true
-                                        IconButton(onClick = { navController.navigate(navItem.path) }) {
-                                            Icon(
-                                                navItem.icon,
-                                                contentDescription = null,
-                                                tint = if (selected) Color.White else LocalContentColor.current
-                                            )
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    ) { innerPadding ->
-                        NavHost(navController, startDestination = "/", Modifier.fillMaxHeight()) {
-                            composable("/") {
+                            ) { innerPadding ->
                                 Books(Modifier.padding(innerPadding)) { bookId ->
                                     navController.navigate("/book/$bookId")
                                 }
                             }
-                            composable("/about-store") {
+                        }
+                        composable("/about-store") {
+                            Scaffold(
+                                topBar = {
+                                    TopAppBar(
+                                        colors = topAppBarColors(
+                                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                            titleContentColor = MaterialTheme.colorScheme.primary,
+                                        ),
+                                        title = { Text("О магазине") }
+                                    )
+                                },
+                                bottomBar = { BottomBar({ navController.navigate(it) }, "/about-store") }
+                            ) { innerPadding ->
                                 AboutStore(Modifier.padding(innerPadding))
                             }
-                            composable("/about-me") {
+                        }
+                        composable("/about-me") {
+                            Scaffold(
+                                topBar = {
+                                    TopAppBar(
+                                        colors = topAppBarColors(
+                                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                            titleContentColor = MaterialTheme.colorScheme.primary,
+                                        ),
+                                        title = { Text("Об авторе") }
+                                    )
+                                },
+                                bottomBar = { BottomBar({ navController.navigate(it) }, "/about-me") }
+                            ) { innerPadding ->
                                 AboutMe(Modifier.padding(innerPadding))
                             }
-                            composable(
-                                "/book/{id}",
-                                arguments = listOf(navArgument("id") { type = NavType.IntType })
-                            ) { navBackStackEntry ->
+                        }
+                        composable(
+                            "/book/{id}",
+                            arguments = listOf(navArgument("id") { type = NavType.IntType })
+                        ) { navBackStackEntry ->
+                            Scaffold(
+                                topBar = {
+                                    TopAppBar(
+                                        colors = topAppBarColors(
+                                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                            titleContentColor = MaterialTheme.colorScheme.primary,
+                                        ),
+                                        title = { Text("Книга") }
+                                    )
+                                },
+                                bottomBar = { BottomBar({ navController.navigate(it) }, "/") }
+                            ) { innerPadding ->
                                 navBackStackEntry.arguments?.let { args ->
                                     BookScreen(args.getInt("id"), Modifier.padding(innerPadding))
                                 }
